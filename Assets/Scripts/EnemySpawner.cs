83e6e5cbca321f4bc76025997958a1bd1f1654b0
Faul69 <melvik99@gmail.com>
2023-04-01 14:32:02 +0300

    Create

---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    private readonly WaitForSeconds TwoSeconds = new WaitForSeconds(2);
+
+    [SerializeField] private Transform _templePoints;
+    [SerializeField] private GameObject _enemy;
+
+    private Transform[] _points;
+
+    private void Start()
+    {
+        _points = new Transform[_templePoints.childCount];
+
+        for (int i = 0; i < _points.Length; i++)
+        {
+            _points[i] = _templePoints.GetChild(i);
+        }
+
+        StartCoroutine(routine: SpawnEnemyOnPoints());
+    }
+
+    private IEnumerator SpawnEnemyOnPoints()
+    {
+        for (int i = 0; i < _points.Length; i++)
+        {
+            yield return TwoSeconds;
+            Instantiate(_enemy, _points[i].transform);
+        }
+    }
+}
+
